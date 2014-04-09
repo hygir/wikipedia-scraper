a8792136618a01d2b21e731810ed5a479a06abf5
Arsen Mamikonyan <esqaw@ytosi.com>
2014-04-09 00:46:38 -0700

    following urls works

--- hywiki/spiders/alpha.py
+++ hywiki/spiders/alpha.py
@@ -1,14 +1,25 @@
-from scrapy.spider import Spider
-from scrapy.selector import Selector
-
+# coding=utf-8
 from hywiki.items import HyWord
+from scrapy.contrib.spiders import CrawlSpider
+from scrapy.contrib.spiders import Rule
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+import re
 
-class HywikiSpider(Spider):
+
+class HywikiSpider(CrawlSpider):
   name = 'alpha'
   allowed_domains = ['hy.wikipedia.org']
   start_urls = [
     'https://hy.wikipedia.org/wiki/Գլխավոր_էջ'
   ]
+  rules = (
+    Rule(SgmlLinkExtractor(), 'parse_word', follow=True),
+  )
 
-  def parse(self, response):
-    sel = Selector(response)
+  word_regex = re.compile(u'[\u0561-\u0585\u0531-\u0556\-]+')
+
+  def parse_word(self, response):
+    for match in self.word_regex.finditer(response.body.decode('utf-8')):
+      word = HyWord()
+      word['text'] = match.group(0)
+      yield word
